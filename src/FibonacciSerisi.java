ad423d2b1945f26ae2d72f322e2803d7286d4c5b
Samet Mete <samet-mete@hotmail.com>
2022-05-09 14:11:24 +0300

    Fibonacci serisi uygulaması eklendi.

---
+++ src/FibonacciSerisi.java
@@ -0,0 +1,17 @@
+public class FibonacciSerisi {
+    public static void main(String[] args) {
+        int number=10,a=0,b=1,c;
+
+        System.out.print(a);
+        System.out.print(" "+b+" ");
+        for(int i=1; i<=number; i++){
+            c = a+b;
+            System.out.print(c);
+
+            a=b;
+            b=c;
+            System.out.print(" ");
+        }
+
+    }
+}
